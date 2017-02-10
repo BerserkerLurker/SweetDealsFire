2dce59f74b0079d30ba09a3c34a86e25899afa77
Kallinikos <lahmar_ahmed@yahoo.com>
2017-02-10 15:14:07 +0100

    SweetDealsFireBased MyAds added -- swipe to delete in Favs done backup.

--- app/src/main/java/com/kallinikos/tech/sweetdealsfire/adapters/FavsListAdapter.java
+++ app/src/main/java/com/kallinikos/tech/sweetdealsfire/adapters/FavsListAdapter.java
@@ -1,21 +1,29 @@
 package com.kallinikos.tech.sweetdealsfire.adapters;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.graphics.drawable.Drawable;
+import android.os.Handler;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.firebase.ui.storage.images.FirebaseImageLoader;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.kallinikos.tech.sweetdealsfire.R;
+import com.kallinikos.tech.sweetdealsfire.app.MainActivity;
 import com.kallinikos.tech.sweetdealsfire.models.AdImage;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -24,7 +32,18 @@ import java.util.List;
 
 public class FavsListAdapter extends RecyclerView.Adapter<FavsListAdapter.MyViewHolder> {
     private static final String TAG = AdsListingAdapter.class.getSimpleName();
+
+
     private List<AdImage> mData;
+    //--------
+    private static final int PENDING_REMOVAL_TIMEOUT = 3000; // 3sec
+    private List<AdImage> itemsPendingRemoval;
+    boolean undoOn;
+    private Handler handler = new Handler(); // hanlder for running delayed runnables
+    HashMap<AdImage, Runnable> pendingRunnables = new HashMap<>(); // map of items to pending runnables, so we can cancel a removal if need be
+    //--------
+
+    private Context mContext;
     private LayoutInflater mInflater;
     private static FavsListAdapter.ClickListener clickListener;
 
@@ -33,6 +52,9 @@ public class FavsListAdapter extends RecyclerView.Adapter<FavsListAdapter.MyView
     }
 
     public FavsListAdapter(Context context, List<AdImage> data){
+        itemsPendingRemoval = new ArrayList<>();
+
+        this.mContext = context;
         this.mData = data;
         this.mInflater = LayoutInflater.from(context);
     }
@@ -46,14 +68,108 @@ public class FavsListAdapter extends RecyclerView.Adapter<FavsListAdapter.MyView
 
     @Override
     public void onBindViewHolder(MyViewHolder holder, int position) {
-        AdImage currentObj = mData.get(position);
-        holder.setData(currentObj);
+        /*AdImage currentObj = mData.get(position);
+        holder.setData(currentObj);*/
+
+        final AdImage currentObj = mData.get(position);
+
+        if (itemsPendingRemoval.contains(currentObj)){
+            // we need to show the "undo" state of the row
+            holder.itemView.setBackgroundColor(Color.RED);
+
+            holder.imgThumb.setVisibility(View.GONE);
+            holder.title.setVisibility(View.GONE);
+            holder.desc.setVisibility(View.GONE);
+            holder.price.setVisibility(View.GONE);
+
+            holder.undoButton.setVisibility(View.VISIBLE);
+            holder.undoButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    // user wants to undo the removal, let's cancel the pending task
+                    Runnable pendingRemovalRunnable = pendingRunnables.get(currentObj);
+                    pendingRunnables.remove(currentObj);
+                    if(pendingRemovalRunnable != null){
+                        handler.removeCallbacks(pendingRemovalRunnable);
+                    }
+                    itemsPendingRemoval.remove(currentObj);
+                    // this will rebind the row in "normal" state
+                    notifyItemChanged(mData.indexOf(currentObj));
+
+                }
+            });
+        }else {
+            // we need to show the "normal" state
+            holder.itemView.setBackgroundColor(Color.WHITE);
+
+            holder.imgThumb.setVisibility(View.VISIBLE);
+            holder.title.setVisibility(View.VISIBLE);
+            holder.desc.setVisibility(View.VISIBLE);
+            holder.price.setVisibility(View.VISIBLE);
+            holder.setData(currentObj);
+
+            holder.undoButton.setVisibility(View.GONE);
+            holder.undoButton.setOnClickListener(null);
+        }
+
+        //holder.setData(currentObj);
     }
+
 
     @Override
     public int getItemCount() {
         return mData.size();
     }
+
+
+    /*public void setUndoOn(boolean undoOn) {
+        this.undoOn = undoOn;
+    }*/
+
+    public boolean isUndoOn() {
+        return true;
+        //return undoOn;
+    }
+
+    public void pendingRemoval(int position){
+        final AdImage obj = mData.get(position);
+        if(!itemsPendingRemoval.contains(obj)){
+            itemsPendingRemoval.add(obj);
+            // this will redraw row in "undo" state
+            notifyItemChanged(position);
+            // let's create, store and post a runnable to remove the item
+            Runnable pendinRemovalRunnable = new Runnable() {
+                @Override
+                public void run() {
+                    remove(mData.indexOf(obj));
+                }
+            };
+            handler.postDelayed(pendinRemovalRunnable, PENDING_REMOVAL_TIMEOUT);
+            pendingRunnables.put(obj, pendinRemovalRunnable);
+        }
+    }
+
+    public void remove(int position){
+        AdImage obj = mData.get(position);
+        if(itemsPendingRemoval.contains(obj)){
+            itemsPendingRemoval.remove(obj);
+        }
+        if(mData.contains(obj)){
+            String key = obj.getKey();
+            String uid = ((MainActivity)mContext).getUid();
+            mData.remove(position);
+            notifyItemRemoved(position);
+
+            DatabaseReference ref = FirebaseDatabase.getInstance().getReference().child("users").child(uid).child("favs").child(key);
+            ref.removeValue();
+        }
+    }
+
+    public boolean isPendingRemoval(int position){
+        AdImage obj = mData.get(position);
+        return itemsPendingRemoval.contains(obj);
+    }
+
 
     public void updateAdapter(String key,String title, int price, String desc, List<String> listImgURL){
         AdImage newCard = new AdImage();
@@ -81,6 +197,7 @@ public class FavsListAdapter extends RecyclerView.Adapter<FavsListAdapter.MyView
         TextView price;
         TextView desc;
         ImageView imgThumb;
+        Button undoButton;
 
 
         Context context;
@@ -90,6 +207,7 @@ public class FavsListAdapter extends RecyclerView.Adapter<FavsListAdapter.MyView
             price = (TextView)itemView.findViewById(R.id.txv_favprice);
             desc = (TextView)itemView.findViewById(R.id.txv_favdesc) ;
             imgThumb = (ImageView)itemView.findViewById(R.id.img_favthumb);
+            undoButton = (Button)itemView.findViewById(R.id.undo_button);
             context = itemView.getContext();
 
             itemView.setOnClickListener(this);
@@ -127,6 +245,7 @@ public class FavsListAdapter extends RecyclerView.Adapter<FavsListAdapter.MyView
                 Glide.with(context)
                         .using(new FirebaseImageLoader())
                         .load(ref)
+                        .override(100,100)
                         .placeholder(R.drawable.loadplaceholder)
                         .animate(android.R.anim.slide_in_left)
                         .error(R.drawable.noimage)
@@ -136,6 +255,7 @@ public class FavsListAdapter extends RecyclerView.Adapter<FavsListAdapter.MyView
 
                 Glide.with(context)
                         .load(R.drawable.noimage)
+                        .override(100,100)
                         .centerCrop()
                         .into(this.imgThumb);
             }
--- app/src/main/java/com/kallinikos/tech/sweetdealsfire/app/FavsListing.java
+++ app/src/main/java/com/kallinikos/tech/sweetdealsfire/app/FavsListing.java
@@ -1,10 +1,15 @@
 package com.kallinikos.tech.sweetdealsfire.app;
 
 
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -32,6 +37,7 @@ public class FavsListing extends Fragment implements Callbackfavs{
     private static final String TAG = AdsFragment.class.getSimpleName();
     private String userId;
     private FavsListAdapter adapter;
+    private RecyclerView recyclerView;
     final private DatabaseReference mref = FirebaseDatabase.getInstance().getReference();
 
     public FavsListing() {
@@ -53,7 +59,7 @@ public class FavsListing extends Fragment implements Callbackfavs{
         }
 
         //Images RecyclerView Setup
-        RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.favsrecyclerView);
+        recyclerView = (RecyclerView) view.findViewById(R.id.favsrecyclerView);
         adapter = new FavsListAdapter(this.getActivity(), new ArrayList<AdImage>());
         recyclerView.setAdapter(adapter);
 
@@ -61,7 +67,8 @@ public class FavsListing extends Fragment implements Callbackfavs{
         mLinearLayoutManagerVertical.setOrientation(LinearLayoutManager.VERTICAL);
         recyclerView.setLayoutManager(mLinearLayoutManagerVertical);
 
-
+        setUpItemTouchHelper();
+        setUpAnimationDecoratorHelper();
         return view;
     }
 
@@ -136,6 +143,157 @@ public class FavsListing extends Fragment implements Callbackfavs{
                 }
             });
         }
+    }
+
+    /**
+     * This is the standard support library way of implementing "swipe to delete" feature. You can do custom drawing in onChildDraw method
+     * but whatever you draw will disappear once the swipe is over, and while the items are animating to their new position the recycler view
+     * background will be visible. That is rarely an desired effect.
+     */
+    private void setUpItemTouchHelper(){
+
+        ItemTouchHelper.SimpleCallback simpleItemTouchCallback = new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
+            //Caching this once
+            Drawable background;
+            boolean initiated;
+
+            private void init(){
+                background = new ColorDrawable(Color.RED);
+                initiated = true;
+            }
+            //No moving motion only left swipe
+            @Override
+            public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
+                return false;
+            }
+
+            @Override
+            public int getSwipeDirs(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder) {
+                int position = viewHolder.getAdapterPosition();
+                if (adapter.isUndoOn() && adapter.isPendingRemoval(position)){
+                    return 0;
+                }
+                return super.getSwipeDirs(recyclerView, viewHolder);
+            }
+
+            @Override
+            public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
+                int swipedPosition = viewHolder.getAdapterPosition();
+                boolean undoOn = adapter.isUndoOn();
+                if(undoOn){
+                    adapter.pendingRemoval(swipedPosition);
+                }else {
+                    adapter.remove(swipedPosition);
+                }
+            }
+
+            @Override
+            public void onChildDraw(Canvas c, RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive) {
+                View itemView = viewHolder.itemView;
+
+                // not sure why, but this method get's called for viewholder that are already swiped away
+                if(viewHolder.getAdapterPosition() == -1){
+                    //not interested in those
+                    return;
+                }
+
+                if(!initiated){
+                    init();
+                }
+
+                //draw red background
+                background.setBounds(itemView.getRight() + (int)dX, itemView.getTop(), itemView.getRight(), itemView.getBottom());
+                background.draw(c);
+
+                super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+            }
+        };
+
+        ItemTouchHelper mItemTouchHelper = new ItemTouchHelper(simpleItemTouchCallback);
+        mItemTouchHelper.attachToRecyclerView(recyclerView);
+    }
+
+    /**
+     * We're gonna setup another ItemDecorator that will draw the red background in the empty space while the items are animating to thier new positions
+     * after an item is removed.
+     */
+    private void setUpAnimationDecoratorHelper(){
+        recyclerView.addItemDecoration(new RecyclerView.ItemDecoration() {
+
+            // we want to cache this and not allocate anything repeatedly in the onDraw method
+            Drawable background;
+            boolean initiated;
+
+            private void init() {
+                background = new ColorDrawable(Color.RED);
+                initiated = true;
+            }
+
+            @Override
+            public void onDraw(Canvas c, RecyclerView parent, RecyclerView.State state) {
+
+                if (!initiated){
+                    init();
+                }
+                //only if animation is in progress
+                if (parent.getItemAnimator().isRunning()){
+                    // some items might be animating down and some items might be animating up to close the gap left by the removed item
+                    // this is not exclusive, both movement can be happening at the same time
+                    // to reproduce this leave just enough items so the first one and the last one would be just a little off screen
+                    // then remove one from the middle
+
+                    // find first child with translationY > 0
+                    // and last one with translationY < 0
+                    // we're after a rect that is not covered in recycler-view views at this point in time
+
+                    View lastViewComingDown = null;
+                    View firstViewComingUp = null;
+
+                    //this is fixed
+                    int left = 0;
+                    int right = parent.getWidth();
+
+                    //this we need to find out
+                    int top = 0;
+                    int bottom = 0;
+
+                    //find relevant translating views
+                    int childCount = parent.getLayoutManager().getChildCount();
+                    for(int i=0; i<childCount; i++){
+                        View child = parent.getLayoutManager().getChildAt(i);
+                        if (child.getTranslationY() < 0) {
+                            // view is coming down
+                            lastViewComingDown = child;
+                        } else if (child.getTranslationY() > 0) {
+                            // view is coming up
+                            if (firstViewComingUp == null) {
+                                firstViewComingUp = child;
+                            }
+                        }
+                    }
+
+                    if (lastViewComingDown != null && firstViewComingUp != null) {
+                        // views are coming down AND going up to fill the void
+                        top = lastViewComingDown.getBottom() + (int) lastViewComingDown.getTranslationY();
+                        bottom = firstViewComingUp.getTop() + (int) firstViewComingUp.getTranslationY();
+                    } else if (lastViewComingDown != null) {
+                        // views are going down to fill the void
+                        top = lastViewComingDown.getBottom() + (int) lastViewComingDown.getTranslationY();
+                        bottom = lastViewComingDown.getBottom();
+                    } else if (firstViewComingUp != null) {
+                        // views are coming up to fill the void
+                        top = firstViewComingUp.getTop();
+                        bottom = firstViewComingUp.getTop() + (int) firstViewComingUp.getTranslationY();
+                    }
+
+                    background.setBounds(left, top, right, bottom);
+                    background.draw(c);
+                }
+
+
+                super.onDraw(c, parent, state);
+            }
+        });
     }
 }
 
